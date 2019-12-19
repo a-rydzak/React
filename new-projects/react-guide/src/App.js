02c54ff8652b2f791b86e186d24f280776d02ea3
Andrew Rydzak <andrew.rydzak@aa.com>
2019-12-19 10:24:35 -0600

    finished non blocking callbacks

--- new-projects/react-guide/src/App.js
+++ new-projects/react-guide/src/App.js
@@ -3,11 +3,14 @@ import Header from './test-components/Header';
 import './App.css';
 
 class App extends Component {
-  render() {
+  // longic here
+  myName = 'Andrew';
+
+  render(myName) {
     return (
       <div className='App'>
         <Header />
-        <h1>Andrew Was Here!</h1>
+        <h1>{this.myName} Was Here!</h1>
       </div>
     );
   }
--- notes-on-operators.js
+++ notes-on-operators.js
@@ -67,3 +67,94 @@ const uniques = arry => new Set(arry);
 const arrayMe = set => Array.from(set);
 
 console.log(arrayMe(uniques(numberMe(filterme(new_str)))));
+
+// ------------------reducer
+const array1 = [1, 2, 3, 4];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+// console.log(array1.reduce(reducer));
+// console.log(array1.reduce(reducer, 5));
+
+//---------------------------------------callback
+
+// const setT = (cb, time) => {
+//   for (let x = 0; x < time * 3; x++) {
+//     continue;
+//   }
+//   cb();
+// };
+
+// setT(function() {
+//   console.log('3');
+// }, 30000000);
+
+// setTimeout(function() {
+//   console.log('2');
+// }, 3000);
+
+// console.log('1');
+
+//-----------------------------------------------Async custom with promises
+
+// const two = (param, param2) => {
+//   const promise = new Promise((resolve, reject) => {
+//     if (param == false) {
+//       reject(33);
+//     }
+//     for (let x = 0; x < 1000000; x++) {
+//       continue;
+//     }
+//     resolve(param2);
+//   });
+//   return promise;
+// };
+
+// two(false)
+//   .then(value => {
+//     console.log(value);
+//   })
+//   .catch(err => {
+//     console.log(`Error ${err}`);
+//   });
+
+// two(true).then(value => {
+//   console.log(value);
+// });
+
+// console.log('1');
+
+// async function getaller() {
+//   await two(true, '1').then(value => {
+//     console.log(value);
+//   });
+//   await two(true, '2').then(value => {
+//     console.log(value);
+//   });
+//   console.log('3');
+// }
+
+// getaller();
+//--------------------------------------------------Non blocking with callbacks and promises
+function reader(textFile, cb) {
+  for (let x = 0; x < 100000 * 3; x++) {
+    continue;
+  }
+  cb(false, 'all your data');
+}
+
+function getter(cb) {
+  promise = new Promise((resolve, reject) => {
+    reader('input.txt', function(err, data) {
+      if (err) return console.error(err);
+      resolve(cb(data));
+    });
+  });
+  return promise;
+}
+
+getter(bb8 => bb8).then(item => {
+  console.log(item);
+  console.log('Webpage Loaded with data');
+});
+
+console.log(1);
