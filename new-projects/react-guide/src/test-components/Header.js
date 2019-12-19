79954d1a76c83ed920c092f5d85e0c7308084086
Andrew Rydzak <andrew.rydzak@aa.com>
2019-12-19 06:57:54 -0600

    added interview algo question

---
+++ new-projects/react-guide/src/test-components/Header.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      // dont use class for className
+      // <div className='Header'>
+      //   <header>
+      //     <ul>
+      //       <h3>This is Not a Header</h3>
+      //       <li>A</li>
+      //       <li>B</li>
+      //       <li>C</li>
+      //     </ul>
+      //   </header>
+      // </div>
+
+      // React.createElement('div', null, 'h1', 'I am not a React app...')
+
+      React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'h1',
+          { className: Header },
+          'I am not a React app...'
+        )
+      )
+    );
+  }
+}
+
+export default Header;
--- notes-on-operators.js
+++ notes-on-operators.js
@@ -1,45 +1,45 @@
 const person = { name: 'Andrew' };
 const newPerson = { ...person, age: '???' };
-console.log(newPerson);
+// console.log(newPerson);
 
 // --------------------------------------Start of nifty arrty operators
 // case 1
 let x = [1, 2, 3];
 let y = [...x];
 x[0] = 44;
-console.log(y);
+// console.log(y);
 
 // vs -------- case 2
 x = [1, 2, 3, 5];
 y = x;
 x[0] = 44;
-console.log(y);
+//console.log(y);
 
 function sorter(...args) {
   return args.sort();
 }
 
-console.log(sorter(1, 2, 3, 4, 5));
+//console.log(sorter(1, 2, 3, 4, 5));
 
 const filterMe = (...args) => {
   return args.filter(el => el > 5);
 };
 
-console.log(filterMe(1, 2, 3, 4, 22));
+// console.log(filterMe(1, 2, 3, 4, 22));
 
 // destructure
 
 [a, b] = [1, 2, 3, 4, 5];
 
-console.log(`From [a, b] = [1, 2, 3, 4, 5]; A is ${a} amnd b is ${b}`);
+// console.log(`From [a, b] = [1, 2, 3, 4, 5]; A is ${a} amnd b is ${b}`);
 
 var numbers = [1, 2, 3, 4, 5];
 
 const doubleMe = numbers.map(item => item * item);
 
-console.log(doubleMe);
+// console.log(doubleMe);
 
-console.log(numbers);
+// console.log(numbers);
 /*
 map()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 find()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
@@ -50,3 +50,20 @@ concat()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/G
 slice()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
 splice()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
 */
+
+// ------------------------------------find the numbers and return
+const strr = '1.33TY,1,2,3,4,5,A';
+
+const new_str = strr.split(',');
+
+const filterme = arry => {
+  return arry.filter(elem => !isNaN(parseInt(elem)));
+};
+
+const numberMe = arry => arry.map(item => parseInt(item));
+
+const uniques = arry => new Set(arry);
+
+const arrayMe = set => Array.from(set);
+
+console.log(arrayMe(uniques(numberMe(filterme(new_str)))));
