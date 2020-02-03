bdd37364bba5722833dc9840c5e0077fd53aded3
Andrew Rydzak <Andrew.Rydzak@coxautoinc.com>
2020-02-03 08:42:59 -0600

    Added css module

--- new-projects/hamburglar/src/components/Layout/Layout.js
+++ new-projects/hamburglar/src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/aux';
+import classes from './layout.module.css';
 const layout = (props) => {
 
     return(
             <Aux>
                 <div>Toolbar, SideDrawer, Backdrop</div>
-                <main>{props.children}</main>
+                <main className={classes.Content}>{props.children}</main>
             </Aux>
     )
 }
