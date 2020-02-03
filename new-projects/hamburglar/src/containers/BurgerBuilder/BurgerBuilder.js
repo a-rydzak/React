ab3fb28eba890170156b7f501bb7410c0da2d452
Andrew Rydzak <Andrew.Rydzak@coxautoinc.com>
2020-02-03 08:32:49 -0600

    components init

---
+++ new-projects/hamburglar/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Component} from 'react';
+import Aux from '../../hoc/aux';
+
+class BurgerBuilder extends Component{
+
+
+
+
+    render(){
+
+        return(<Aux>
+            <div>Burger</div>
+            <div>Build Controls</div>
+        </Aux>)
+    }
+
+}
+
+export default BurgerBuilder;
